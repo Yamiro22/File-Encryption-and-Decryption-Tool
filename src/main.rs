d40628f8be400fee084cae11a365b1700e6a2d7a
Yamiro22 <136946777+Yamiro22@users.noreply.github.com>
2023-11-14 19:06:56 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,69 @@
 use std::fs;
 use std::env;
-use openssl::symm::{encrypt, decrypt, Cipher};
+use openssl::symm::{Crypter, Mode, Cipher};
 use openssl::hash::{hash, MessageDigest};
 use std::ffi::OsString;
 use std::os::unix::ffi::OsStrExt;
 use log::error;
+use rand::Rng;
+use openssl::rand::rand_bytes;
+use anyhow::{Result, Context};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<OsString> = env::args_os().collect();
-    match args.as_slice() {
-        [_, command, key, filename] => {
-            let command = command.to_str().ok_or("Invalid command")?;
-            let key = key.to_str().ok_or("Invalid key")?;
-            let filename = filename.to_str().ok_or("Invalid filename")?;
-
-            // Derive the key using PBKDF2 to ensure it is 32 bytes long
-            let mut key_bytes = [0u8; 32];
-            let salt = [0u8; 16]; // Use a random salt for added security
-            let iterations = 10000; // Choose an appropriate number of iterations
-            openssl::pkcs5::pbkdf2_hmac(key.as_bytes(), &salt, iterations, MessageDigest::sha256(), &mut key_bytes)?;
-
-            let data = fs::read(filename)?;
-
-            let cipher = Cipher::aes_256_cbc();
-
-            let result = match command {
-                "encrypt" => encrypt(cipher, &key_bytes, None, &data)?,
-                "decrypt" => decrypt(cipher, &key_bytes, None, &data)?,
-                _ => {
-                    return Err(format!("Invalid command: {}", command).into());
-                }
-            };
-
-            let new_filename = format!("{}_new", filename);
-            fs::write(new_filename, result)?;
-        },
-        _ => {
-            return Err("Usage: <encrypt|decrypt> <key> <file>".into());
-        }
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if let [_, command, key, filename] = args.as_slice() {
+        let command = parse_command(command)?;
+        let key_bytes = derive_key(key)?;
+        let data = fs::read(filename)?;
+        let cipher = Cipher::aes_256_cbc();
+        let result = match command {
+            Command::Encrypt => encrypt_data(cipher, &key_bytes, &data)?,
+            Command::Decrypt => decrypt_data(cipher, &key_bytes, &data)?,
+        };
+        fs::write(filename, &result)?;
+    } else {
+        return Err("Usage: <encrypt|decrypt> <key> <file>".into());
     }
-    
     Ok(())
+}
+
+fn parse_command(command: &str) -> Result<Command> {
+    match command {
+        "encrypt" => Ok(Command::Encrypt),
+        "decrypt" => Ok(Command::Decrypt),
+        _ => Err(format!("Invalid command: {}", command).into()),
+    }
+}
+
+fn derive_key(key: &str) -> Result<[u8; 32]> {
+    let mut key_bytes = [0u8; 32];
+    let salt: [u8; 16] = rand::thread_rng().gen();
+    let iterations = 100000;
+    openssl::pkcs5::pbkdf2_hmac(key.as_bytes(), &salt, iterations, MessageDigest::sha256(), &mut key_bytes)?;
+    Ok(key_bytes)
+}
+
+fn encrypt_data(cipher: Cipher, key: &[u8; 32], data: &[u8]) -> Result<Vec<u8>> {
+    let mut encrypter = Crypter::new(cipher, Mode::Encrypt, key, Some(&[0u8; 16]))?;
+    encrypter.pad(false);
+    let mut result = vec![0; data.len() + cipher.block_size()];
+    let count = encrypter.update(data, &mut result)?;
+    let final_result = encrypter.finalize(&mut result[count..])?;
+    result.truncate(count + final_result);
+    Ok(result)
+}
+
+fn decrypt_data(cipher: Cipher, key: &[u8; 32], data: &[u8]) -> Result<Vec<u8>> {
+    let mut decrypter = Crypter::new(cipher, Mode::Decrypt, key, Some(&[0u8; 16]))?;
+    decrypter.pad(false);
+    let mut result = vec![0; data.len() + cipher.block_size()];
+    let count = decrypter.update(data, &mut result)?;
+    let final_result = decrypter.finalize(&mut result[count..])?;
+    result.truncate(count + final_result);
+    Ok(result)
+}
+
+enum Command {
+    Encrypt,
+    Decrypt,
 }
